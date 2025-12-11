3eed9b5200b3d57ac6c6e7616ad21110c76fc853
Ananya Jain <132234981+ananyajainn@users.noreply.github.com>
2025-12-11 09:44:36 -0500

    Update client.py
    format for readability

--- client.py
+++ client.py
@@ -3,20 +3,6 @@ import threading
 import sys
 import argparse
 
-
-# TODO: Implement a client that connects to your server to chat with other clients here
-  
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     s.sendall(b"Hello, world")
-#     data = s.recv(1024)
-
-# print(f"Received {data!r}")
-
-# HOST = "127.0.0.1"
-# PORT = 65431 
-
 parser = argparse.ArgumentParser(prog='TCP Chatroom Client')
 parser.add_argument("-join", action="store_true")
 parser.add_argument("-host")
